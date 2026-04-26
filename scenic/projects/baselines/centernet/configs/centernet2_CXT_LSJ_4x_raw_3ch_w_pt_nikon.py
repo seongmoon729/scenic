4725e7113834334599d0eea1581d9ecc7425b15f
seongmoon729 <jsm21star@gmail.com>
2026-04-26 12:18:24 +0000

    update

---
+++ scenic/projects/baselines/centernet/configs/centernet2_CXT_LSJ_4x_raw_3ch_w_pt_nikon.py
@@ -0,0 +1,25 @@
+# Copyright 2026 The Scenic Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""raw_3ch_w_pt + nod_nikon target: small-object dominant test set."""
+
+from scenic.projects.baselines.centernet.configs.centernet2_CXT_LSJ_4x_raw_3ch_w_pt import get_config as get_base_config
+
+
+def get_config():
+  config = get_base_config()
+  # nikon test median object ~22x22 in 256² → ~88x88 in 1024² model space.
+  # Bias training scales toward smaller objects; trim the high end.
+  config.dataset_configs.scale_range = (0.1, 1.5)
+  return config
---
+++ scenic/projects/baselines/centernet/configs/centernet2_CXT_LSJ_4x_raw_3ch_w_pt_pascalraw.py
@@ -0,0 +1,26 @@
+# Copyright 2026 The Scenic Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""raw_3ch_w_pt + pascalraw target: large-object dominant test set."""
+
+from scenic.projects.baselines.centernet.configs.centernet2_CXT_LSJ_4x_raw_3ch_w_pt import get_config as get_base_config
+
+
+def get_config():
+  config = get_base_config()
+  # pascalraw test median object ~87x87 in 256² → ~348x348 in 1024² model space.
+  # Drop the small-object end and shift high so COCO objects can be zoomed
+  # into pascalraw scale during training.
+  config.dataset_configs.scale_range = (1.0, 4.0)
+  return config
